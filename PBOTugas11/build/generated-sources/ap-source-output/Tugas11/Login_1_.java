9e3e1e671b400ab8fbcbe35a43db187d6ed33713
erikhotunnajwa06 <erikhotunnajwaaghnia@gmail.com>
2025-11-12 16:29:09 +0700

    Add files via upload

---
+++ PBOTugas11/build/generated-sources/ap-source-output/Tugas11/Login_1_.java
@@ -0,0 +1,14 @@
+package Tugas11;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-12T12:52:07", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Login_1.class)
+public class Login_1_ { 
+
+    public static volatile SingularAttribute<Login_1, String> password;
+    public static volatile SingularAttribute<Login_1, String> username;
+
+}
